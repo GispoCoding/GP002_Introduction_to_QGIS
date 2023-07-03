b236a891bd403ec09ef7cd7ea1c6e08cc512f06c
Juho Ervasti <118284595+JuhoErvasti@users.noreply.github.com>
2023-07-03 11:02:06 +0300

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -6,6 +6,6 @@ rm -rf /out/*
 
 Rscript -e "bookdown::render_book('/app', 'bookdown::html_book')"
 
-mv _book/* /out/
-rm -r _book
+mv /app/_book/* /out/
+rm -r /app/_book
 chmod --recursive 777 /out
